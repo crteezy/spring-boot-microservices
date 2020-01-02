8f117dc85eed5933ffcb51d390f63107308eb978
Chamithra Thenuwara <43117071+crteezy@users.noreply.github.com>
2020-01-02 14:52:41 +0530

    working with RestTemplate

--- game-catalog-service/src/main/java/com/demo/controller/GameCatalogController.java
+++ game-catalog-service/src/main/java/com/demo/controller/GameCatalogController.java
@@ -1,23 +1,41 @@
 package com.demo.controller;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.demo.model.Catalog;
+import com.demo.model.Game;
+import com.demo.model.Rating;
 
 @Controller
 @RequestMapping("/catalog")
 public class GameCatalogController {
+	
+	@Autowired
+	private RestTemplate template;
 
 	@RequestMapping("/{userId}")
 	@ResponseBody
 	public List<Catalog> getCatalog(@PathVariable String userId) {
+		
+		List<Rating> ratings = Arrays.asList(
+				new Rating(1L, 1L, 4.5),
+				new Rating(2L, 2L, 4.0)
+		);
+		
+		return ratings.stream().map(rating -> {
+			Game game = template.getForObject("http://localhost:8082/games/" + rating.getGameId(), Game.class);
+			return new Catalog(1L, game.getName(), "Desc", rating.getRate());
+		}).collect(Collectors.toList());
 
-		return Collections.singletonList(new Catalog(1L, "Catalog 1", "Description 1", 4.0));
+//		return Collections.singletonList(new Catalog(1L, "Catalog 1", "Description 1", 4.0));
 	}
 }
