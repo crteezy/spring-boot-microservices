141b98cc810a4266407dbb153a9ab2c0b572afab
Chamithra Thenuwara <43117071+crteezy@users.noreply.github.com>
2020-01-02 22:27:40 +0530

    working with WebClient

--- game-catalog-service/src/main/java/com/demo/controller/GameCatalogController.java
+++ game-catalog-service/src/main/java/com/demo/controller/GameCatalogController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.demo.model.Catalog;
 import com.demo.model.Game;
@@ -21,10 +22,14 @@ public class GameCatalogController {
 	
 	@Autowired
 	private RestTemplate template;
+	
+	@Autowired
+	private WebClient.Builder builder;
 
 	@RequestMapping("/{userId}")
 	@ResponseBody
 	public List<Catalog> getCatalog(@PathVariable String userId) {
+		
 		
 		List<Rating> ratings = Arrays.asList(
 				new Rating(1L, 1L, 4.5),
@@ -32,8 +37,20 @@ public class GameCatalogController {
 		);
 		
 		return ratings.stream().map(rating -> {
-			Game game = template.getForObject("http://localhost:8082/games/" + rating.getGameId(), Game.class);
+			
+			// RestTemplate
+//			Game game = template.getForObject("http://localhost:8082/games/" + rating.getGameId(), Game.class);
+			
+			// WebClient
+			Game game = builder.build()
+			.get()
+			.uri("http://localhost:8082/games/" + rating.getGameId())
+			.retrieve()
+			.bodyToMono(Game.class)
+			.block();
+			
 			return new Catalog(1L, game.getName(), "Desc", rating.getRate());
+			
 		}).collect(Collectors.toList());
 
 //		return Collections.singletonList(new Catalog(1L, "Catalog 1", "Description 1", 4.0));
