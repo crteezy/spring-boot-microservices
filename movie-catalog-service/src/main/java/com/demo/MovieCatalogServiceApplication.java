61ed5d2c941d0983662fca3e50d8c321dd908c76
Chamithra Thenuwara <43117071+crteezy@users.noreply.github.com>
2020-01-04 00:34:50 +0530

    Update MovieCatalogServiceApplication.java

--- movie-catalog-service/src/main/java/com/demo/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/demo/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -15,7 +16,10 @@ public class MovieCatalogServiceApplication {
 	@Bean // to make it singleton
 	@LoadBalanced // for Eureka client
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		// Setting timeouts
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 	
 	@Bean // to make it singleton
